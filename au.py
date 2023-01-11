d928fac5741e2c97f5875685b80e587cbba43b4f
Carlos Lopes <67564891+carloshplopes@users.noreply.github.com>
2023-01-11 09:12:56 +0000

    Adicionando o acendimento de LED manual - sleep

---
+++ au.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+from time import sleep
+LED1_PIN = 11 #P1 - pede boleia e acende o LED1
+LED2_PIN = 13 #P2 - pede boleia e acende o LED2
+LED3_PIN = 15 #P3
+LED4_PIN = 29 #P1
+LED5_PIN = 31 #P2
+LED6_PIN = 33 #P3 - pede boleia e acende o LED6
+LED7_PIN = 36 #P1
+LED8_PIN = 38 #P2
+LED9_PIN = 40 #P3
+
+#GPIO numbering scheme 
+#BOARD - pin numbers of the Pi header
+#BMC   - GPIO numbers (depends on the Raspberry PI version)
+GPIO.setmode(GPIO.BOARD)
+
+#GPIO channel setup
+#IN  - input
+#OUT - output
+GPIO.setup(LED1_PIN,GPIO.OUT)
+GPIO.setup(LED2_PIN,GPIO.OUT)
+GPIO.setup(LED3_PIN,GPIO.OUT)
+GPIO.setup(LED4_PIN,GPIO.OUT)
+GPIO.setup(LED5_PIN,GPIO.OUT)
+GPIO.setup(LED6_PIN,GPIO.OUT)
+GPIO.setup(LED7_PIN,GPIO.OUT)
+GPIO.setup(LED8_PIN,GPIO.OUT)
+GPIO.setup(LED9_PIN,GPIO.OUT)
+
+#GPIO channel state
+#HIGH  - True or 1
+#LOW   - False or 0
+for i in range(1):
+    # pedidos de boleia
+    GPIO.output(LED1_PIN,GPIO.HIGH)
+    GPIO.output(LED2_PIN,GPIO.HIGH)
+    GPIO.output(LED3_PIN,GPIO.LOW)
+    GPIO.output(LED4_PIN,GPIO.LOW)
+    GPIO.output(LED5_PIN,GPIO.LOW)
+    GPIO.output(LED6_PIN,GPIO.HIGH)
+    GPIO.output(LED7_PIN,GPIO.LOW)
+    GPIO.output(LED8_PIN,GPIO.LOW)
+    GPIO.output(LED9_PIN,GPIO.LOW)
+    sleep(5)
+    # P1 e P2 entra no OBU e começa a viagem
+    GPIO.output(LED1_PIN,GPIO.LOW)
+    GPIO.output(LED2_PIN,GPIO.LOW)
+    sleep(10)
+    # P2 entra no OBU e começa a viagem
+    GPIO.output(LED6_PIN,GPIO.LOW)
+    sleep(15)
+    # chegada na escola
+    GPIO.output(LED7_PIN,GPIO.HIGH)
+    GPIO.output(LED8_PIN,GPIO.HIGH)
+    GPIO.output(LED9_PIN,GPIO.HIGH)
+    sleep(5)
+GPIO.cleanup()
